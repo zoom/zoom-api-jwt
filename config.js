15b2df88d188f57e8b2fbdb8ae833dfd8a51aeb7
Shrijana Ghimire <shrijanaghimire@Shrijana-Ghimire.local>
2019-03-27 14:56:58 -0700

    Changed the env option to production

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV || 'development'
 
-//insert your API Key & Secret for each environment, keep this file local and never push it to repo.
+//insert your API Key & Secret for each environment, keep this file local and never push it to a public repo for security purposes.
 const config = {
 	development :{
 		APIKey : '',
